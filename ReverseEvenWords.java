e5699cbc453ea4d1b970588c8462d6829056177e
Hemapriya14 <99642541+Hemapriya14@users.noreply.github.com>
2022-02-16 07:44:02 -0800

    Add files via upload

---
+++ ReverseEvenWords.java
@@ -0,0 +1,14 @@
+package weeek2.day2;
+
+public class ReverseEvenWords {
+
+	public static void main(String[] args) {
+		String test="I am a software tester";
+		String[] words = test.split(" ");
+		for(int i=0;i<words.length; i++);{
+		System.out.println(words);
+
+		}
+	}
+
+}
